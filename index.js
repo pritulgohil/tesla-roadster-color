1df6c2968538117898eb2d3af05281987246ffef
pritulgohil <34166907+pritulgohil@users.noreply.github.com>
2023-03-10 23:03:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,208 @@
+
+   document.querySelectorAll(".box")[0].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t1.jpeg");
+        document.querySelectorAll(".box")[0].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+
+    document.querySelectorAll(".box")[1].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t2.jpeg");
+        document.querySelector("p").innerHTML = "Midnight Silver Metallic";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";    });
+
+    document.querySelectorAll(".box")[2].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t3.jpeg");
+        document.querySelector("p").innerHTML = "Obsidian Black Metallic";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+
+    document.querySelectorAll(".box")[3].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t4.jpeg");
+        document.querySelector("p").innerHTML = "Deep Blue Metallic";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+    document.querySelectorAll(".box")[4].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t5.jpeg");
+        document.querySelector("p").innerHTML = "Silver Metallic";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+
+    document.querySelectorAll(".box")[5].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t6.jpeg");
+        document.querySelector("p").innerHTML = "Pearl White Multi-Coat";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+    document.querySelectorAll(".box")[6].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t7.jpeg");
+        document.querySelector("p").innerHTML = "Red Multi-Coat";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+
+    document.querySelectorAll(".box")[7].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t8.jpeg");
+        document.querySelector("p").innerHTML = "Very Orange Multi-Coatc";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+    document.querySelectorAll(".box")[8].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t9.jpeg");
+        document.querySelector("p").innerHTML = "Brilliant Yellow Multi-Coat";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+
+    document.querySelectorAll(".box")[9].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t10.jpeg");
+        document.querySelector("p").innerHTML = "Glacier Blue Multi-Coat";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+    document.querySelectorAll(".box")[10].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t11.jpeg");
+        document.querySelector("p").innerHTML = "Electric Blue Multi-Coat";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1.5)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1)";
+    });
+
+
+    document.querySelectorAll(".box")[11].addEventListener("click",function(){
+        document.getElementById("change").setAttribute("src", "images/t12.jpeg");
+        document.querySelector("p").innerHTML = "Green Lotus Multi-Coat";
+        document.querySelectorAll(".box")[0].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[1].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[2].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[3].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[4].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[5].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[6].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[7].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[8].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[9].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[10].style.transform = "scale(1)";
+        document.querySelectorAll(".box")[11].style.transform = "scale(1.5)";
+    });
